aa47fdc5832a719c46902bd340870bd0dc569861
ToffePeer1 <contact@toffepeer.net>
2026-06-15 17:17:21 +0200

    Prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,14 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: [
-    'dist',
-    'build',
-    'node_modules',
-    'supabase',
-    '**/.venv/**'
-  ] },
+  { ignores: ['dist', 'build', 'node_modules', 'supabase', '**/.venv/**'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     linterOptions: {
       reportUnusedDisableDirectives: true,
@@ -33,14 +25,12 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
     },
   },
-);
+  prettier
+)
